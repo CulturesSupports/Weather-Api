cf1d41782ef493eafaa9bbd645e169f62d5f83ae
Culture.Support <huxoz@protonmail.com>
2025-01-10 22:42:46 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
+
+
 document.addEventListener('DOMContentLoaded', getWeather);
 
 async function getWeather() {
-    const apiKey = '62e18d2ce2f1e208703f3cff17a58aba
-';
+    const apiKey = '62e18d2ce2f1e208703f3cff17a58aba';
     const city = 'Ålesund';
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    const weatherIcons = {
+        '01d': 'Clear sky (day)', 
+        '01n': 'Clear sky (night)',
+        '02d': 'Few clouds (day)', 
+        '02n': 'Few clouds (night)',
+        '03d': 'Scattered clouds (day)', 
+        '03n': 'Scattered clouds (night)',
+        '04d': 'Broken clouds (day)', 
+        '04n': 'Broken clouds (night)',
+        '09d': 'Shower rain (day)', 
+        '09n': 'Shower rain (night)',
+        '10d': 'Rain (day)', 
+        '10n': 'Rain (night)',
+        '11d': 'Thunderstorm (day)', 
+        '11n': 'Thunderstorm (night)',
+        '13d': 'Snow (day)', 
+        '13n': 'Snow (night)',
+        '50d': 'Mist (day)', 
+        '50n': 'Mist (night)'
+    };
+
+    const iconList = document.getElementById('icon-list');
+    for (const [icon, description] of Object.entries(weatherIcons)) {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `<img class="icon" src="http://openweathermap.org/img/wn/${icon}.png" alt="${description}"> ${description}`;
+        iconList.appendChild(listItem);
+    }
 
     try {
         const response = await fetch(apiUrl);
