21f33c46c66350d3c69dce258e87b12f34bcd860
Culture.Support <huxoz@protonmail.com>
2025-01-10 22:57:46 +0100

    Update and rename app.js to scripts.js

---
+++ scripts.js
@@ -0,0 +1,29 @@
+const apiKey = '62e18d2ce2f1e208703f3cff17a58aba
+';
+const apiUrl = 'api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt={cnt}&appid={API key}'";
+const locationInput = document.getElementById('locationInput');
+const searchButton = document.getElementById('searchButton');
+const locationElement = document.getElementById('location');
+const temperatureElement = document.getElementById('temperature');
+const descriptionElement = document.getElementById('description');
+
+searchButton.addEventListener('click', () => {
+  const location = locationInput.value;
+  if (location) {
+    fetchWeather(location);
+  }
+});
+
+function fetchWeather(location) {
+  const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`;
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      locationElement.textContent = data.name;
+      temperatureElement.textContent = `${Math.round(data.main.temp)}°C`;
+      descriptionElement.textContent = data.weather[0].description;
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
